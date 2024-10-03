58b2da640b858265b360856b770eba715f20f8f4
PeterHW963 <peterhadiwijaya963@gmail.com>
2024-10-03 23:19:46 +0800

    Add card with tasks

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,14 @@ function App() {
   return (
     <div>
       <AppNavbar expanded={expanded} setExpanded={setExpanded} />
-      <div style={{ paddingTop: expanded ? '120px' : '0', transition: 'padding-top 0.25s', transitionTimingFunction: 'ease'}}>   
+      <div style={{ paddingTop: expanded ? '121px' : '0', transition: 'padding-top 0.24s', transitionTimingFunction: 'ease'}}>   
         <div className='welcomeMsg'>Welcome!</div>
         <Tasks />
+        <div className='buttons'>
+          <button className="w-100 btn btn-outline-secondary btn-sm" type="button">Back</button>
+          <button className="w-100 btn btn-outline-primary btn-sm" type="button">Add</button>
+          <button className="w-100 btn btn-outline-danger btn-sm" type="button">Dismiss</button>
+        </div>
       </div>
     </div>
   )
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 function Tasks() {
   return (
     <div>
-      <h1>Tasks</h1>
+      <div className="div text-center card border-dark">
+        <div className="bg-dark text-light card-header">
+          <h3 className="my-1">John Doe</h3>
+        </div>
+        <div className="card-body">
+          <div className="list-group list-group-flush">
+            <div className="div text-danger fw-bold list-group-item">You have 3 alerts today!</div>
+            <div className="list-group-item">
+              <li>Attend Mass at 9am.</li>
+              <li>Submit retreat registration form by 5pm.</li>
+              <li>Attend ministry meeting at 8pm.</li>
+            </div>  
+          </div>
+        </div>
+        
+      </div>
     </div>
   )
 }
